ae043c1e475d7d0a1803327e50450ac1cc47bb95
Khairullayev Baglan <bagahairollaev@gmail.com>
2026-07-06 19:55:18 +0500

    Day 2: Created first FastAPI server and about endpoint

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Welcome to UNT AI!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to ENT AI!"}
+
+@app.get("/about")
+def about():
+    return {"message": "This project helps students prepare for UNT!"}
