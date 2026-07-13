38490e5aa475f52e76f99e61b2e1fd14ad63aa4a
Khairullayev Baglan <bagahairollaev@gmail.com>
2026-07-13 20:32:52 +0500

    Day 8: Start SQLite database integration

--- app/main.py
+++ app/main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 from app.routes import router
 from app.register import router as register_router
 
+from app.database import engine
+from app.models import Base
+
+from app.routes import router
+from app.register import router as register_router
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(router)
 app.include_router(register_router)
