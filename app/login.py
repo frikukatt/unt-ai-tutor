362c785caa9a4212555b6625b438d00bba1a9ce3
Khairullayev Baglan <bagahairollaev@gmail.com>
2026-07-27 19:47:18 +0500

    Day 13: implement JWT authentication

--- app/login.py
+++ app/login.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import User
 from app.users import UserLogin
-from app.security import verify_password
+from app.security import verify_password, create_access_token
 
 router = APIRouter()
 
@@ -13,6 +13,10 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user.email).first()
     if not db_user or not verify_password(user.password, db_user.password):
         raise HTTPException(status_code=400, detail="Invalid email or password")
+    access_token = create_access_token(
+    data={ "sub": db_user.email }
+)
     return {
-    "message": "Login successful"
+    "access_token": access_token,
+    "token_type": "bearer"
 }
--- app/security.py
+++ app/security.py
@@ -1,4 +1,6 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +11,20 @@ def hash_password(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+SECRET_KEY = "31e21ed7c051d175f1283571a663514b8cd0bfc18cc95cb888b088c3ab0f57d3"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
