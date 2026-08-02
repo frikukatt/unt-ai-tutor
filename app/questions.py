e57e12a32d64122fa7a0713095da5c3541658b56
Khairullayev Baglan <bagahairollaev@gmail.com>
2026-08-02 19:35:44 +0500

    Day 17: Complete Question CRUD API

--- app/questions.py
+++ app/questions.py
@@ -4,6 +4,7 @@ from app.schemas import QuestionCreate
 from app.models import Question
 from sqlalchemy.orm import Session
 from app.database import get_db
+from fastapi import HTTPException
 
 
 router = APIRouter()
@@ -31,3 +32,51 @@ def get_questions(
         query = query.filter(Question.topic == topic)
 
     return query.all()
+
+@router.get("/questions/{question_id}")
+def get_question(question_id: int, db: Session = Depends(get_db)):
+    question = db.query(Question).filter(Question.id == question_id).first()
+
+    if question is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Question not found"
+        )
+    return question
+
+@router.put("/questions/{question_id}")
+def update_question(
+    question_id: int,
+    question_data: QuestionCreate,
+    db: Session = Depends(get_db)
+):
+    question = db.query(Question).filter(Question.id == question_id).first()
+
+    if question is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Question not found"
+        )
+    for key, value in question_data.model_dump().items():
+        setattr(question, key, value)
+
+    db.commit()
+    db.refresh(question)
+    return question
+
+@router.delete("/questions/{question_id}")
+def delete_question(
+    question_id: int,
+    db: Session = Depends(get_db)
+):
+    question = db.query(Question).filter(Question.id == question_id).first()
+
+    if question is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Question not found"
+        )
+
+    db.delete(question)
+    db.commit()
+    return {"message": "Question deleted successfully"}
